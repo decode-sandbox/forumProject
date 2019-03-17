f31062c82daa695f8f5b57d8f415929fbf9a9562
SANNY Abdul-Qadir <qadirsanny@gmail.com>
2019-03-17 05:54:08 +0100

    mise de point du projet pour commencer le travail. Rdirection d'url, ajout de tampate dans le settings.py ajout d'un petit contenue de base dans base.html

--- forum/urls.py
+++ forum/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('forum/',include('forum.urls')),
+    path('',views.home),
+    path('base',views.base),
 ]
--- forum/views.py
+++ forum/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request,'forum/home.html')
+
+def base(request):
+	return render(request, 'base.html')
