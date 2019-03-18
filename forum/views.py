e6fd5eea6df824f6a775408dcfe321c5ad637d98
SANNY Abdul-Qadir <qadirsanny@gmail.com>
2019-03-18 10:20:09 +0100

    syuff

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request,'forum/home.html')
+	postes = ['rouge', 'orange', 'jaune', 'vert', 'bleu', 'indigo',
+'violet']
+	cats = ['TECHNO', 'POLITIQUE', 'GAMING', 'DROLE', 'BRICOLAGE', 'DEV']
+
+	return render(request,'forum/home.html', locals())
 
 def base(request):
+	
 	return render(request, 'base.html')
+
+def postesOfCategorie(request, catID):
+	return render('categories.html')
