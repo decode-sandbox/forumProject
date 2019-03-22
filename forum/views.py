dda6130fcd41dbc5e6ce86a16f3df6b40ffb14eb
anicetEnselme <enselme20@gmail.com>
2019-03-22 19:17:53 +0100

    Prise en compte des reviews

--- forum/views.py
+++ forum/views.py
@@ -7,18 +7,12 @@ def home(request):
 
 	return render(request,'forum/home.html', locals())
 
-def base(request):
-	
-	return render(request, 'base.html')
-
 def postesOfCategorie(request, catID):
 	return render('categories.html')
 
 def login(request):
 	return render(request,'forum/login.html')
-<<<<<<< HEAD
 
 def register(request):
 	return render(request,'forum/register.html')
-=======
->>>>>>> c471ecd9856d00c0963cd44e7d0dddac3b0b5401
+
