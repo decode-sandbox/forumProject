410d619dae419a3b6a583fd967811d6b9d404d0f
Walter <michaelwalter299@gmail.com>
2019-03-23 15:18:10 +0100

    ce que j'ai fais

---
+++ forumProject/forum/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def home(request):
+	postes = ['rouge', 'orange', 'jaune']
+	cats = ['TECHNO', 'POLITIQUE', 'GAMING', 'DROLE', 'BRICOLAGE', 'DEV']
+
+	return render(request,'forum/home.html', locals())
+
+
+def postesOfCategorie(request, catID):
+	return render('categories.html')
+
+def Poste(request):
+        return render(request,'forum/Poste.html')
+
+def coP(request):
+        return render(request,'forum/coP.HTML')
+
+def comment(request):
+        return render(request,'forum/comment.html')
